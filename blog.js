a6f0133f274702f13708d96ed4b4f155cf8f7d59
Jasmine Ng <jasmineng2005@gmail.com>
2024-10-28 00:53:47 -0700

    milestone 1 updates

--- blog.js
+++ blog.js
@@ -3,17 +3,17 @@ var blogs = [
         title: "Blog #1",
         date: "10/27/2024",
         description: "First blog ever!",
-        image: "./images/minionselfie.png",
+        image: "minionselfie.png",
         imageAlt: "Minion taking selfie.",
-        slug: "https://youtu.be/dQw4w9WgXcQ?si=FzgDhj32G1P34rF4"
+        slug: "./blog/blog1.html",
     },
     {
         title: "Blog #2",
         date: "10/28/2024",
         description: "Second blog ever!",
-        image: "./images/minionselfie.png",
+        image: "minionselfie.png",
         imageAlt: "Minion taking selfie.",
-        slug: "https://youtu.be/dQw4w9WgXcQ?si=FzgDhj32G1P34rF4"
+        slug: "./blog/blog2.html",
     },
 ];
 var blogContainer = document.getElementById("blog-container");
@@ -23,12 +23,21 @@ blogs.forEach(function (blog) {
     var blogTitle = document.createElement("h1");
     blogTitle.textContent = blog.title;
     blogDiv.appendChild(blogTitle);
-    var blogImage = document.createElement("img");
-    blogImage.src = blog.image;
-    blogImage.alt = "Image for ${blog.title}";
-    blogDiv.appendChild(blogImage);
+    var blogDate = document.createElement("p");
+    blogDate.textContent = blog.date;
+    blogDiv.appendChild(blogDate);
     var blogDescription = document.createElement("p");
     blogDescription.textContent = blog.description;
     blogDiv.appendChild(blogDescription);
+    var blogImage = document.createElement("img");
+    blogImage.src = blog.image;
+    blogImage.alt = "Image for ${blog.title}";
+    blogImage.width = 350;
+    blogImage.height = 300;
+    blogDiv.appendChild(blogImage);
+    var blogSlug = document.createElement("a");
+    blogSlug.href = blog.slug;
+    blogSlug.textContent = "Read more";
+    blogDiv.appendChild(blogSlug);
     blogContainer && blogContainer.appendChild(blogDiv); //if it is not null, run
 });
--- blog.ts
+++ blog.ts
@@ -1,50 +1,65 @@
 type Blog = {
-    title: string;
-    date: string;
-    description: string;
-    image: string; //(ex. “./images/me.png”)
-    imageAlt: string;
-    slug: string; //a slug is a URL name used to redirect to a specific page
+  title: string;
+  date: string;
+  description: string;
+  image: string; //(ex. “./images/me.png”)
+  imageAlt: string;
+  slug: string; //a slug is a URL name used to redirect to a specific page
 };
 
 const blogs: Blog[] = [
-    {
-        title: "Blog #1",
-        date: "10/27/2024",
-        description: "First blog ever!",
-        image: "./images/minionselfie.png",
-        imageAlt: "Minion taking selfie.",
-        slug: "https://youtu.be/dQw4w9WgXcQ?si=FzgDhj32G1P34rF4"
-    },
-    {
-        title: "Blog #2",
-        date: "10/28/2024",
-        description: "Second blog ever!",
-        image: "./images/minionselfie.png",
-        imageAlt: "Minion taking selfie.",
-        slug: "https://youtu.be/dQw4w9WgXcQ?si=FzgDhj32G1P34rF4"
-    },
+  {
+    title: "Blog #1",
+    date: "10/27/2024",
+    description: "First blog ever!",
+    image: "minionselfie.png",
+    imageAlt: "Minion taking selfie.",
+    slug: "./blog/blog1.html",
+  },
+  {
+    title: "Blog #2",
+    date: "10/28/2024",
+    description: "Second blog ever!",
+    image: "minionselfie.png",
+    imageAlt: "Minion taking selfie.",
+    slug: "./blog/blog2.html",
+  },
 ];
-
 
 const blogContainer = document.getElementById("blog-container");
 
-blogs.forEach(blog => {
-	const blogDiv = document.createElement("div");
-    blogDiv.className = "blog-post";
+blogs.forEach((blog) => {
+  const blogDiv = document.createElement("div");
+  blogDiv.className = "blog-post";
 
-    const blogTitle = document.createElement("h1");
-    blogTitle.textContent = blog.title;
-    blogDiv.appendChild(blogTitle);
 
-    const blogImage = document.createElement("img");
-    blogImage.src = blog.image;
-    blogImage.alt = "Image for ${blog.title}";
-    blogDiv.appendChild(blogImage);
+  const blogTitle = document.createElement("h1");
+  blogTitle.textContent = blog.title;
+  blogDiv.appendChild(blogTitle);
 
-    const blogDescription = document.createElement("p");
-    blogDescription.textContent = blog.description; 
-    blogDiv.appendChild(blogDescription);
 
-    blogContainer&&blogContainer.appendChild(blogDiv); //if it is not null, run
+  const blogDate = document.createElement("p");
+  blogDate.textContent = blog.date;
+  blogDiv.appendChild(blogDate);
+
+
+  const blogDescription = document.createElement("p");
+  blogDescription.textContent = blog.description;
+  blogDiv.appendChild(blogDescription);
+
+
+  const blogImage = document.createElement("img");
+  blogImage.src = blog.image;
+  blogImage.alt = "Image for ${blog.title}";
+  blogImage.width = 350;
+  blogImage.height = 300;
+  blogDiv.appendChild(blogImage);
+
+  
+  const blogSlug = document.createElement("a");
+  blogSlug.href = blog.slug;
+  blogSlug.textContent = "Read more";
+  blogDiv.appendChild(blogSlug);
+
+  blogContainer && blogContainer.appendChild(blogDiv); //if it is not null, run
 });
