832a36c815d9ffc1a9b071bd69af170f073bde0c
MI <jasmineng2005@gmail.com>
2025-06-09 00:48:22 -0700

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/jasmiineng.github.io">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
